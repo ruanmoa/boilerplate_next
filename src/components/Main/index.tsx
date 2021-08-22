c12ba17793e142e1776ce9e1c35464a790ab33f4
Ruan Moraes Andrade <ruan.moa@gmail.com>
2021-08-22 17:03:31 -0300

    Finalizando Boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Logo circular do next com um N em cor branca com fundo preto"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com sódigo"
+    />
+  </S.Wrapper>
 )
 
 export default Main
